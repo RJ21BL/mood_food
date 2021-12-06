460d5fc4536567f82aeab8e3fa97c7c05d1c399c
Rebecca <Beckyrose200@aol.com>
2021-12-06 17:04:58 +0000

    Database added

---
+++ spec/controllers/food_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe FoodController, type: :controller do
+  describe "GET /new " do
+    it "responds with 200" do
+      get :new
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "POST /" do
+    it "responds with 200" do
+      post :create, params: { post: { message: "Hello, world!" } }
+      expect(response).to redirect_to(posts_url)
+    end
+  end
+
+  describe "GET /" do
+    it "responds with 200" do
+      get :index
+      expect(response).to have_http_status(200)
+    end
+  end
+end
