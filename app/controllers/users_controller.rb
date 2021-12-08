a1c0930546730e1d8d04257e1dae81b6beae1cd3
Rebecca <Beckyrose200@aol.com>
2021-12-08 22:19:59 +0000

    Signing in and Out

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to '/food#index', notice: "Thank you for signing up!"
+    else
+      render "new"
+    end
+  end
+
+  def index 
+    # if params[:search_by_user] != ""
+    #   @user_searched = User.where("lower(first_name) = ?", params[:search_by_user].downcase)
+    # end 
+  end 
+
+  private
+
+  def user_params
+   params.require(:user).permit(:first_name, :surname, :email, :password, :password_confirmation, :gender, :dob)
+  end
+end
