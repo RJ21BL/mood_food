8cedc66664f128c06b1da0fcffb11c0e5d6fd442
Richard Lawrence <richard.lawrence@simplybusiness.co.uk>
2021-12-09 09:38:27 +0000

    Changed ingredients column data type. Seeded database.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_08_174117) do
+ActiveRecord::Schema.define(version: 2021_12_09_092123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 2021_12_08_174117) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "cooking_instructions", default: "{}"
-    t.string "ingredients", default: [], array: true
+    t.text "ingredients", default: "{}"
     t.string "allergies", default: [], array: true
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,20 @@
 puts 'Seeding...'
-Food.create(dietary_pref:'vegan', ingredients:'lentils scotch bonnet plantain and rice and peas', cooking_instructions:'fry lentils with scotch bonnet and plantain, then boil rice and peas', cooking_time: 30)
+Food.create([{
+  dietary_pref: 'Vegan', 
+  ingredients: 'Lentils scotch bonnet plantain and rice and peas', 
+  cooking_instructions: 'Fry lentils with scotch bonnet and plantain, then boil rice and peas', 
+  cooking_time: 30
+},
+{
+  dietary_pref: 'Carnivore',
+  ingredients: 'Chicken, jerk seasoning, salt, vinegar',
+  cooking_instructions: 'Wash chicken with salt and white vinegar, then poke holes into chicken and marinate with jerk seasoning.',
+  cooking_time: 60
+},
+{
+  dietary_pref: 'Vegatarian',
+  ingredients: 'Samon, mixed vegetables, sweet potatoes, all purpose seasoning, salt, pepper and honey',
+  cooking_instructions: 'Descale samon, marinate with all purpose seasoning, thyme and honey and put in oven. Boil vegetables. Marinate sweet potatoes with salt and pepper, and put in oven.',
+  cooking_time: 60
+}])
 puts 'Seeding done.'
