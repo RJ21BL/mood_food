9a93e4630e72d6d3a8ad65c8aba8f7df67a56d56
Richard Lawrence <richard.lawrence@simplybusiness.co.uk>
2021-12-10 10:46:53 +0000

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,13 @@
 puts 'Seeding...'
 
 @ingredients = ['Lentils', 'Scotch bonnet', 'Plantain', 'rice and peas']
+@cooking_instructions = ['Fry lentils with scotch bonnet and plantain, then boil rice and peas']
 
 Food.create([{
   name: 'Plant power',
   dietary_pref: 'Vegan', 
   ingredients: @ingredients,
-  cooking_instructions: 'Fry lentils with scotch bonnet and plantain, then boil rice and peas', 
+  cooking_instructions: @cooking_instructions,
   cooking_time: 30,
   allergies: 'None'
 },
