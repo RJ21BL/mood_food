7917d059893ae404708821b9d32b24f5df27359f
Richard Lawrence <richard.lawrence@simplybusiness.co.uk>
2021-12-13 17:24:29 +0000

    about us page with two short stories

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'login', to: 'sessions#new', as: 'login'
   get 'sign_up', to: 'users#new', as: 'sign_up'
-  get 'logout', to: 'sessions#destroy', as: 'logout'
-  root to: 'sessions#new'
-  
+  get 'logout', to: 'sessions#destroy', as: 'logout'  
   
   resources :food
   resources :users
   resources :sessions
+  resources :about_us
 end
