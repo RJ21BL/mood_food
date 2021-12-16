8c9c99fa7514c2c21a69bcd30eb29a84cc05dff5
Rebecca <Beckyrose200@aol.com>
2021-12-16 14:41:05 +0000

    q

--- app/controllers/food_controller.rb
+++ app/controllers/food_controller.rb
@@ -62,15 +62,16 @@ class FoodController < ApplicationController
 
   def search
     if params[:search_by_recipe] != "" 
-      @recipe_searched = Food.where("name LIKE ?", params[:search_by_recipe])
-      @search_option = 1
+      @recipe_searched = Food.includes((:name).(params[:search_by_recipe]))
+      # @recipe_searched = Food.includes("name LIKE ?", params[:search_by_recipe])
+      # @search_option = 1
       if @recipe_searched == []
         @recipe_searched = Food.all
-        @search_option = 0
+        # @search_option = 0
       end
     end 
   end 
-
+  # references(:name).search(params[:search_by_recipe])
  
 
   def recipe
@@ -83,6 +84,4 @@ private
     params.require(:food).permit(:name, :dietary_pref, :ingredients, :cooking_time, :allergies, :cooking_instructions)
   end
 
-  def fav_params
-    params.require(:food).permit(:id)
 end
